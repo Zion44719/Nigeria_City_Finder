c8ec17f3d7455c0eda802f3abeb7f155f1a27496
zion44719 <zionctech@gmail.com>
2022-07-27 17:42:10 +0100

    first commit

---
+++ effect.js
@@ -0,0 +1,52 @@
+"use strict";
+// get input element
+
+const searchInput = document.querySelector(".searchBtn");
+const suggestionCon = document.querySelector(".suggestionList");
+
+// Event handlers
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
+
+// get API
+const citiesStates = [];
+
+async function fetchAPi() {
+  const res = await fetch(
+    "https://gist.githubusercontent.com/Zion44719/3cab67fd551c3ac80e88826cf3d1d933/raw/d8a4656ef4edffe913d7623bce3c234fdfb00805/Cities_database.json"
+  );
+  const data = await res.json();
+  citiesStates.push(...data);
+}
+
+// To find match word typed
+function findMatches(wordToMatch, citiesStates) {
+  return citiesStates.filter((searchName) => {
+    const regX = new RegExp(wordToMatch, "gi");
+    return searchName.city.match(regX) || searchName.admin_name.match(regX);
+  });
+}
+
+// Display Matches Function
+function displayMatches() {
+  const findArray = findMatches(this.value, citiesStates);
+
+  const matchEl = findArray
+    .map((place) => {
+      const regX = new RegExp(this.value, "gi");
+      const cityName = place.city.replace(
+        regX,
+        `<span class="highlight">${this.value}</span>`
+      );
+
+      const stateName = place.admin_name.replace(
+        regX,
+        `<span class="highlight">${this.value}</span>`
+      );
+      return `<li class="name">${cityName}, ${stateName}</li>`;
+    })
+    .join("");
+  suggestionCon.innerHTML = matchEl;
+}
+
+fetchAPi();
