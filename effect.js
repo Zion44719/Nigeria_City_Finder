d2cb747c9aaad5512e2391015e1a56fcb74382b9
zion44719 <zionctech@gmail.com>
2022-07-28 12:35:26 +0100

    to display selected city or state

--- effect.js
+++ effect.js
@@ -3,6 +3,7 @@
 
 const searchInput = document.querySelector(".searchBtn");
 const suggestionCon = document.querySelector(".suggestionList");
+const displayField = document.querySelector(".displayField");
 
 // Event handlers
 searchInput.addEventListener("change", displayMatches);
@@ -14,9 +15,7 @@ const citiesStates = [];
 // api "https://gist.githubusercontent.com/Zion44719/3cab67fd551c3ac80e88826cf3d1d933/raw/d8a4656ef4edffe913d7623bce3c234fdfb00805/Cities_database.json"
 
 async function fetchAPi() {
-  const res = await fetch(
-    "https://gist.githubusercontent.com/Zion44719/3cab67fd551c3ac80e88826cf3d1d933/raw/d8a4656ef4edffe913d7623bce3c234fdfb00805/Cities_database.json"
-  );
+  const res = await fetch("Cities_database.json");
   const data = await res.json();
   citiesStates.push(...data);
 }
@@ -45,10 +44,28 @@ function displayMatches() {
         regX,
         `<span class="highlight">${this.value}</span>`
       );
-      return `<li class="name">${cityName}, ${stateName}</li>`;
+      return `<li class="name" onclick="displayCityInfo(this)" id="${place.city}">${cityName}, ${stateName}</li>`;
     })
     .join("");
   suggestionCon.innerHTML = matchEl;
+}
+
+// Display The Selected State or City
+function displayCityInfo(cityField) {
+  let city = cityField.id;
+  const place = citiesStates.find((place) => place.city === city);
+  displayField.innerHTML = `
+  <ul>
+  <h2>Details</h2>
+  <li><b>Country:</b> ${place.country}</li>
+  <li><b>State:</b> ${place.admin_name}</li>
+  <li><b>City:</b> ${place.city}</li>
+  <li><b>Latitude:</b> ${place.lat}</li>
+  <li><b>Longitude:</b> ${place.lng}</li>
+  <li><b>Population:</b> ${place.population}</li>
+  </ul>
+  `;
+  console.log(place);
 }
 
 fetchAPi();
